7ee83801d858202d4affc502a585e13698babce2
Pritamhsusjs <21052855@kiit.ac.in>
2024-03-11 01:43:47 +0530

    commit

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-pink-300 py-4">
       <div className="container mx-auto text-center">
-        <p className="text-white">© 2024 Your Company. All rights reserved.</p>
+        <p className="text-white">© 2024 TaazaKhabar. All rights reserved.</p>
       </div>
     </footer>
   );
